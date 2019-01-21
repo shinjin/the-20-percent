a0b96758b5c96b7b937dd4360e69860abf430cac
Rick Shin <shinjin@users.noreply.github.com>
2019-01-21 13:38:36 -0500

    Update graph

--- python3/breadth_first_search.py
+++ python3/breadth_first_search.py
@@ -2,12 +2,14 @@
 from collections import deque
 
 
-graph = {'A': {'B', 'C'},
-         'B': {'A', 'D', 'E'},
-         'C': {'A', 'F'},
-         'D': {'B'},
-         'E': {'B', 'F'},
-         'F': {'C', 'E'}}
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
 
 
 def bfs(graph, start):
