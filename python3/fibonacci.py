ac92e60707846081fae1913b735b4b77b52e4a7c
Rick Shin <shinjin@users.noreply.github.com>
2019-01-23 13:00:07 -0500

    Update fibonacci.py

--- python3/fibonacci.py
+++ python3/fibonacci.py
@@ -1,5 +1,4 @@
-""" Two fibonacci sequence implementations: a naive implementation using
-    recursion and an optimized version
+""" Four fibonacci sequence implementations, from worst to best.
 """
 
 
@@ -8,6 +7,32 @@ def fibonacci_naive(n):
         return n
 
     return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+cache = {}
+
+def fibonacci_cache(n):
+    if n == 0 or n == 1:
+        return n
+
+    if n in cache:
+        return cache[n]
+
+    cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+
+    return cache[n]
+
+
+cache = {}
+
+def fibonacci_iter(n):
+    cache[0] = 0
+    cache[1] = 1
+
+    for i in range(2, n + 1):
+        cache[i] = cache[i - 1] + cache[i - 2]
+
+    return cache[n]
 
 
 def fibonacci(n):
